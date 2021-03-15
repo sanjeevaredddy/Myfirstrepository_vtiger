792c50c6b2dcc0ebb8466452c9d744247a6e0b9e
admin <admin@sanjureddy>
2021-03-15 13:36:27 +0530

    Added new contact

---
+++ SDET11_VTIGER_JENKINS/src/main/java/com/comcast/objutil/Contact.java
@@ -0,0 +1,12 @@
+package com.comcast.objutil;
+
+import org.testng.annotations.Test;
+
+public class Contact {
+	@Test
+	public void Createcontact()
+	{
+		System.out.println("Create contact");
+	}
+
+}
