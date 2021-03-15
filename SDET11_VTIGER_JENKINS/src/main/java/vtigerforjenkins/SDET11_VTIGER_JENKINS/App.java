89c647e19f971610fdc170e883ff0d4aad512275
admin <admin@sanjureddy>
2021-03-15 12:56:04 +0530

    Please commit the framework...

---
+++ SDET11_VTIGER_JENKINS/src/main/java/vtigerforjenkins/SDET11_VTIGER_JENKINS/App.java
@@ -0,0 +1,13 @@
+package vtigerforjenkins.SDET11_VTIGER_JENKINS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SDET11_VTIGER_JENKINS/src/test/java/com/comcast/contacttest/Contacttest.java
@@ -0,0 +1,22 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class Contacttest {
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("Execute createcontacttest");
+	}
+	@Test
+	public void modifycontacttest()
+	{
+		System.out.println("Execute modifycontacttest");
+	}
+	@Test
+	public void deletecontacttest()
+	{
+		System.out.println("Execute deletecontacttest");
+	}
+
+}
